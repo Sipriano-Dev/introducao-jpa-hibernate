21e9c9d3684656c32a1cb6b0c266b5c67020d2fb
Anderson Sipriano <Andsipriano@gmail.com>
2025-03-26 17:25:51 -0300

    Entity Pessoa mapeada, EntityManager e EntityManageFactory implementados

--- src/main/java/com/sipriano/aulajpa/Main.java
+++ src/main/java/com/sipriano/aulajpa/Main.java
@@ -2,14 +2,29 @@ package com.sipriano.aulajpa;
 
 import com.sipriano.aulajpa.dominio.Pessoa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa(1, "Carlos da Silva", "Carlos@gmail.com");
-        Pessoa p2 = new Pessoa(1, "Joaquim Silva", "Joaquim@gmail.com");
-        Pessoa p3 = new Pessoa(1, "Ana Maria SAntos", "Maria@gmail.com");
+        Pessoa p1 = new Pessoa(null, "Carlos da Silva", "Carlos@gmail.com");
+        Pessoa p2 = new Pessoa(null, "Joaquim Silva", "Joaquim@gmail.com");
+        Pessoa p3 = new Pessoa(null, "Ana Maria SAntos", "Maria@gmail.com");
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        EntityManager em = null;
+        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa")) {
+            em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(p1);
+            em.persist(p2);
+            em.persist(p3);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Pronto!");
+
     }
 }
